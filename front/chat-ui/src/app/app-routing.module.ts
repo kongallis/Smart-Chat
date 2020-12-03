0bdbf1fe92ff779916b11b578f3b2fc21d7a0b9a
Konstantinos <kongallis@gmail.com>
2020-12-04 00:47:39 +0200

    Login Page blueprint

--- front/chat-ui/src/app/app-routing.module.ts
+++ front/chat-ui/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  // { path: 'second-component', component: SecondComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' }, // redirect to `first-component`
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
